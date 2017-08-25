c5c37c6baf0e6f5a949ca61bd53719e2e109a59c
PaulGFriesen <friesen3@msu.edu>
2017-08-25 10:31:26 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 
 /*
-
+tvShow class to give some properties to a T.V Shows
 */
 
-public class tvshow {
+public class tvShow{
 
   public static void main(String[] args){
-
+    
   }//end main
 }//end class tvshow
