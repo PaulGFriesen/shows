8d01402d4d225c836d5407355485ebccfae291d5
PaulGFriesen <friesen3@msu.edu>
2017-08-25 10:47:04 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,22 @@ tvShow class to give some properties to a T.V Shows
 */
 
 public class tvShow{
-
+  
+  String showGenre
+  String showName;
+  Integer numSeasons;  
+  
+  //A tvShow has a name, number of seasons and a genre
+  public tvShow(String name ,Integer seasons, String genre){
+    seasons = numSeasons  
+    name = showName
+    genre = showGenre
+  }//end 
+  
+  public void displaySeasons(){
+    System.out.println(name + "has " + seasons + " seasons");
+  }//end displaySeasons
+  
   public static void main(String[] args){
     
   }//end main
